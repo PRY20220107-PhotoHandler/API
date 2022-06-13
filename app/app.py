b5a48c4be887ef4acd75fa25ddb8b188f6e9a75c
Geral <hokaide@outlook.com>
2022-06-12 19:31:55 -0500

    oks

--- app/app.py
+++ app/app.py
@@ -1,27 +1,12 @@
 from flask import Flask, request, render_template
-from model import hola
 import requests
 
 app=Flask(__name__)
-
-@app.route('/api/<section>', methods=['GET'])
-def api(section):
-    hola(section)
-    return request.host_url + "/static/results/00000.jpg"
     
 @app.route('/')
 def index():
     data = { 'title':'Photo Handler' }
     return render_template('index.html', data=data)
-
-@app.route('/result/<type>')
-def results(type):
-    url = "http://127.0.0.1:5000/api/" + type
-    payload={}
-    headers = {}
-    response = requests.request("GET", url, headers=headers, data=payload)
-    data = { 'title':'Photo Handler', 'image': response.text }
-    return render_template('result.html', data=data)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
--- app/static/js/layout.js
+++ app/static/js/layout.js
@@ -1,13 +1,33 @@
 function getResult() {
     var fileInput = document.getElementById('imageInput');
     var filePath = fileInput.value;
-    var allowedExtensions = /(\.jpg|\.jpeg|\.png|\.gif)$/i;
+    var allowedExtensions = /(\.jpg|\.png)$/i;
+    var obj = {};
     if (!allowedExtensions.exec(filePath)) {
-        alert('Debe subir una imagen con el formato jpg, jpeg, png o gif');
+        alert('Debe subir una imagen con el formato jpg o png');
         fileInput.value = '';
     } else {
-        var e = document.getElementById("typeSelect");
-        var strType = e.value;
-        window.location.href = "http://127.0.0.1:5000/result/"+strType;
+        var e = document.getElementById("textInput");
+        var strText = e.value;
+        if (strText == "") {
+            alert('Debe ingresar un texto para modificar la imagen');
+            return;
+        }
+        var formdata = new FormData();
+        formdata.append("image", fileInput.files[0], fileInput.value);
+        formdata.append("text", strText);
+        var requestOptions = { method: 'POST', body: formdata, redirect: 'follow' };
+        fetch("http://a1ae-34-90-22-27.ngrok.io/edit", requestOptions)
+            .then(response => response.text())
+            .then(result => showResult(result))
+            .catch(error => console.log('error', error));
+        document.getElementById("loading-screen").style.display = "block";
+        document.getElementById("photo-form").style.display = "none";
     }
+}
+
+function showResult(obj) {
+    document.getElementById("loading-screen").style.display = "none";
+    document.getElementById("resultImage").src = JSON.parse(obj).message;
+    document.getElementById("results").style.display = "block";
 }
